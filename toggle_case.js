b55f08892209c356f4ab2dbb6961fbea45caba7c
vasubhardwaj011 <vasubhardwaj011@gmail.com>
2024-07-05 18:12:34 +0530

    change variable name

--- toggle_case.js
+++ toggle_case.js
@@ -1,16 +1,16 @@
 // Write a C program to toggle case of each character of a string
 
-let string_a = "vasubhardwaj011@gmail.com";
+let input = "vasubhardwaj011@gmail.com";
 
 let text = "";
 
-for (let i = 0; i < string_a.length; i++) {
+for (let i = 0; i < input.length; i++) {
     
-    if (string_a[i] == string_a[i].toUpperCase()) {
-        text += string_a[i].toLowerCase();
+    if (input[i] == input[i].toUpperCase()) {
+        text += input[i].toLowerCase();
 
     } else {
-        text += string_a[i].toUpperCase();
+        text += input[i].toUpperCase();
         
     }
 }
