b722d414b2c3093e4405e5e33f23893a219fc56d
vasubhardwaj011 <vasubhardwaj011@gmail.com>
2024-07-05 16:33:24 +0530

    create new javaScript file toggle_case.js

---
+++ toggle_case.js
@@ -0,0 +1,15 @@
+let string_a = "vasubhardwaj011@gmail.com";
+
+let text = "";
+
+for (let i = 0; i < string_a.length; i++) {
+    
+    if (string_a[i] == string_a[i].toUpperCase()) {
+        text += string_a[i].toLowerCase();
+
+    } else {
+        text += string_a[i].toUpperCase();
+        
+    }
+}
+console.log(text);
