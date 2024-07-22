4ed08502072cb001886bdbf865c311de50f6cbca
vasubhardwaj011 <vasubhardwaj011@gmail.com>
2024-07-22 18:05:49 +0530

    feat: update 23_remove_last_occurrence_of_character.js

--- 23_remove_last_occurrence_of_character.js
+++ 23_remove_last_occurrence_of_character.js
@@ -3,30 +3,26 @@
 
 let text = {
     string: "My Name is Vasu",
-    search: "a",
-    count: 0
+    search: "a"
 }
 
 for (let i = 0; i < text.string.length; i++) {
 
     if (text.string[i] == text.search) {
-        text.count++;
-        if (text.count == text.count) {
-            text.last_occurrence_char_index = i;
-        }
+        text.last_occurrence_char_index = i;
 
     }
 }
 
 // let a = text.string.slice(0,text.last_occurrence_char_index);
 // let b = text.string.slice((text.last_occurrence_char_index + 1),text.string.length);
-// text["last removed text"] = a + b;
+// text.string = a + b;
 
 //           |
 //          _|_         // Upper code is written at bottom in a single line.
 //          \ /
 //           V
 
-text["removed_text_from_last"] = text.string.slice(0,text.last_occurrence_char_index) + text.string.slice((text.last_occurrence_char_index + 1),text.string.length);
+text.string = text.string.slice(0, text.last_occurrence_char_index) + text.string.slice((text.last_occurrence_char_index + 1), text.string.length);
 
 console.log(text);
