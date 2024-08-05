f9d0e5a2200d43f06fe571213f3e9f51fe69e201
vasubhardwaj011 <vasubhardwaj011@gmail.com>
2024-08-05 16:29:57 +0530

    refactor: update code to avoid inbuilt functions and refactor logic
    - Replaced previous implementation that used replace method
    - Implemented a new approach to construct the string without using the replace method

--- 24_remove_all_occurrence_of_character.js
+++ 24_remove_all_occurrence_of_character.js
@@ -4,15 +4,24 @@
 let text = {
     string: "My Name is Vasuaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
     search: "a",
-    replace_with: ""
+    replace_with: "",
+    newString: ""
 }
+
+// for (let i = 0; i < text.string.length; i++) {
+
+//     if (text.string[i] == text.search) {
+
+//         text.string = text.string.replace(text.string[i], text.replace_with);
+//         i = 0;
+
+//     }
+// }
 
 for (let i = 0; i < text.string.length; i++) {
 
-    if (text.string[i] == text.search) {
-
-        text.string = text.string.replace(text.string[i], text.replace_with);
-        i = 0;
+    if (text.string[i] !== text.search) {
+        text.newString += text.string[i];
 
     }
 }
